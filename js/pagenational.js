cca2fe8d0fcbe51b6f0c018fbcafcfb086938b94
lulu <1334080443@qq.com>
2018-11-16 11:54:52 +0800

    完成分页条点击效果，存在bug 页面按钮最后一个被点击的时候无法阻止默认事件

--- js/pagenational.js
+++ js/pagenational.js
@@ -15,21 +15,66 @@
         getHospitalListBycondition('.container .group');
         allPageCount(getHospitalListBycondition('.container .group'));
         currentPageActive(1);
+        template(getHospitalListBycondition('.container .group'),0);
         e.preventDefault();
     });
     //页面按钮被点击执行高亮，并且返回当前页面值
     $('.page-wrap').on('click',function getCurrent(e){
-        // debugger;
         currentPageActive($(e.target).text());
         template(getHospitalListBycondition('.container .group'),$(e.target).text()-1);
         return false;
     });
+
+    //点击上下页切换
+    $('.pagenation .item-pre').on('click',function(){
+        current = $('.pagenation').find('.page_active').text()-1;
+        if(current===0){
+            current =4;
+        }
+        currentPageActive(current);
+        template(getHospitalListBycondition('.container .group'),current-1);
+        return false;
+    });
+
+    $('.pagenation .item-next').on('click',function(){
+        current =parseInt($('.pagenation').find('.page_active').text())+1;
+        console.log(current)
+        if(current>Math.round(getHospitalListBycondition('.container .group').length/3)){
+            current =1;
+        }
+        currentPageActive(current);
+        template(getHospitalListBycondition('.container .group'),current-1);
+        return false;
+    });
+    //点击首位页按钮
+    $('.pagenation .item-first').on('click',function(){
+        current = 1;
+        currentPageActive(current);
+        template(getHospitalListBycondition('.container .group'),current-1);
+        return false;
+    });
+
+    $('.pagenation .item-last').on('click',function(){
+        current = Math.round(getHospitalListBycondition('.container .group').length/3);
+        currentPageActive(current);
+        template(getHospitalListBycondition('.container .group'),current-1);
+        return false;
+    });
+
+     //输入页数进入相应的页面
+    $('.pagenation .input-submit').on('click',function(){
+        current = $('.pagenation .input-page').val();
+        currentPageActive(current);
+        template(getHospitalListBycondition('.container .group'),current-1);
+        return false;
+    });
+
     //分页条的页数确定
     function allPageCount(arr){
         var length = Math.round(arr.length/3);
         $('.page-wrap').empty();
         for(var index = 1;index<length+1;index++){
-            var el = $('<a href="" class="page">'+index+'</a>');
+            var el = $('<span class="page">'+index+'</span>');
             $('.page-wrap').append(el);
         }
     }
@@ -37,6 +82,17 @@
     function currentPageActive(current){
         $('.page').removeClass('page_active').eq(current-1).addClass('page_active');
     }
+
+
+
+
+
+
+
+
+
+
+
 
 //  通过筛选出的信息进行展示展示
     function getHospitalListBycondition(selector){
@@ -52,21 +108,24 @@
         $(".hospital-list").empty();
         hospitalListGroup = hospitalListGroup.slice(1);
         var index = 0;
-        for(index = current*3 ;index < current*3+3;index++){
-            var el = $("<div class=\"hospital-group\">\n" +
-                "            <img src=\""+hospitalListGroup[index][6]+"\" >\n" +
-                "            <div class=\"message\">\n" +
-                "                <div class=\"name\">\n" +
-                "                    <span class=\"hospital-name\">"+hospitalListGroup[index][3]+"</span>\n" +
-                "                    <span class=\"hospital-level\">"+hospitalListGroup[index][1]+"</span>\n" +
-                "                </div>\n" +
-                "                <div class=\"time\">"+hospitalListGroup[index][7]+"</div>\n" +
-                "                <div class=\"phone\">"+hospitalListGroup[index][5]+"</div>\n" +
-                "                <div class=\"address\">"+hospitalListGroup[index][4]+"</div>\n" +
-                "                <a href=\"\" class=\"button\">&nbsp;</a>\n" +
-                "            </div>");
-            $(".hospital-list").append(el);
+        if(hospitalListGroup.length>=1){
+            for(index = current*3 ;index < current*3+3;index++){
+                var el = $("<div class=\"hospital-group\">\n" +
+                    "            <img src=\""+hospitalListGroup[index][6]+"\" >\n" +
+                    "            <div class=\"message\">\n" +
+                    "                <div class=\"name\">\n" +
+                    "                    <span class=\"hospital-name\">"+hospitalListGroup[index][3]+"</span>\n" +
+                    "                    <span class=\"hospital-level\">"+hospitalListGroup[index][1]+"</span>\n" +
+                    "                </div>\n" +
+                    "                <div class=\"time\">"+hospitalListGroup[index][7]+"</div>\n" +
+                    "                <div class=\"phone\">"+hospitalListGroup[index][5]+"</div>\n" +
+                    "                <div class=\"address\">"+hospitalListGroup[index][4]+"</div>\n" +
+                    "                <a href=\"\" class=\"button\">&nbsp;</a>\n" +
+                    "            </div>");
+                $(".hospital-list").append(el);
+            }
         }
+
 
     }
 
